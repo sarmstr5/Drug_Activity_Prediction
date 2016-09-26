30485e3c4abf50ffff77cb968da47ea298477175
sarmstr5 <git.sarmstr5@gmail.com>
2016-09-25 21:03:46 -0400

    Added comments on next steps

---
+++ main.py
@@ -0,0 +1,49 @@
+# read in the data
+# partition/startify the data
+# do i need to do amything about the different columns?
+# test the releveance of the columns
+from pylab import *
+import pandas as pd
+import numpy as np
+
+def feature_selection():
+    pass
+
+def evaluate_model():
+    pass
+
+def graph_ROC():
+    pass
+
+def f1_scoring():
+    pass
+
+def naive_baise():
+    pass
+
+def kNN():
+    pass
+
+def classify():
+    pass
+
+def read_in_train():
+
+    pass
+
+if __name__ == '__main__':
+    training_file = 'train_drugs.txt'
+    # need to clean data first
+    # can i visualize data?
+    df = pd.read_csv(training_file)
+    # need to do something about the tabs
+    print(df.head())
+
+    # first step is to figure out what the data is like
+    # i should first check columns first
+    # can i see what the columns represent?
+    # how do i do the bayes? do i have to break them up into ranges? i can use //
+    # 
+
+
+
