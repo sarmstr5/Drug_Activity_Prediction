639414a8bcc60d1f9480a131c5dec83592b88e3a
sarmstr5 <git.sarmstr5@gmail.com>
2016-09-29 21:45:15 -0400

    Created seperate files for pos's and neg's from training data to deal with unbalanced classes

--- clean_drug_training.py
+++ clean_drug_training.py
@@ -9,16 +9,21 @@ NUMBER_OF_PARAMETERS = 100000
 def training_to_list(file):
     print("converting training data to list")
     max_param_count = 0
-    param_list = []
-    bind_list = []
+    pos_param_list = []
+    neg_param_list = []
+    pos_bind_list = []
+    neg_bind_list = []
     with open(file, 'r') as csv:
         param_num = 0
         for row in csv:
             row_list = row.split('\t')
-            # for column in row_list[1]:
-            bind_list.append(int(row_list[0]))
-            param_list.append(row_list[1])      #list of strings that represent column location of bools
-    return bind_list, param_list
+            if row_list[0] == 0:
+                neg_bind_list.append(int(row_list[0]))
+                neg_param_list.append(row_list[1])      #list of strings that represent column location of bools
+            else:
+                pos_bind_list.append(int(row_list[0]))
+                pos_param_list.append(row_list[1])      #list of strings that represent column location of bools
+    return neg_bind_list, neg_param_list, pos_bind_list, pos_param_list
 
 def test_to_list(file):
     print("converting training data to list")
@@ -57,40 +62,53 @@ def create_csr(param_lists, row_lists, value_lists, num_rows):
 
     return sparse_csr
 # t = training data, x = test data
-def write_file_to_disk(csr_t, y, csr_x,fn_t, fn_y, fn_x):
-    print('file to disk')
-    np.savez(fn_t, data=csr_t.data, indices=csr_t.indices, indptr=csr_t.indptr, shape=csr_t.shape)
-    np.savez(fn_x, data=csr_x.data, indices=csr_x.indices, indptr=csr_x.indptr, shape=csr_x.shape)
-    with open(fn_y, 'w') as csv:
-        for binding in y:
-            csv.write("{0}\n".format(binding))
+def write_csr_to_disk(csr, fn,):
+    print('file {0} to disk'.format(fn))
+    np.savez(fn, data=csr.data, indices=csr.indices, indptr=csr.indptr, shape=csr.shape)
+
+def write_list_to_disk(list, fn):
+    with open(fn, 'w') as csv:
+        for row in list:
+            csv.write("{0}\n".format(row))
+
+def split_classes(train_csr, y):
+    train_df =
 
 def main():
     print('Starting File')
     full_run = False
     if(full_run == True):
-        filename = 'train_drugs'
+        filename_pos = 'train_drugs_pos'
+        filename_neg = 'train_drugs_neg'
         training_file = 'train_drugs.txt'
-        filename_y = 'train_drugs_bindings'
+        filename_y_pos = 'train_drugs_bindings_pos'
+        filename_y_neg = 'train_drugs_bindings_neg'
         filename_test = 'test_drugs'
         test_file = 'test.txt'
     else:
-        filename = 'train_drugs_short'
+        filename_pos = 'train_drugs_short_pos'
+        filename_neg = 'train_drugs_short_neg'
         training_file = 'train_drugs_short.txt'
-        filename_y = 'train_drugs_bindings_short'
+        filename_y_pos = 'train_drugs_bindings_short_pos'
+        filename_y_neg = 'train_drugs_bindings_short_neg'
         filename_test = 'test_drugs_short'
         test_file = 'test_short.txt'
 
     # convert training and test data to lists
-    binding_vector, jagged_data_list = training_to_list(training_file)
+    neg_binding_vector, neg_jagged_data_list, pos_binding_vector, pos_jagged_data_list = training_to_list(training_file)
     jagged_test_list = test_to_list(test_file)
 
     # convert lists to csrs
-    training_csr = jagged_list_to_csr(jagged_data_list)
+    neg_training_csr = jagged_list_to_csr(neg_jagged_data_list)
+    pos_training_csr = jagged_list_to_csr(pos_jagged_data_list)
     test_csr = jagged_list_to_csr(jagged_test_list)
 
     # write cleaned data to file
-    write_file_to_disk(training_csr, binding_vector, test_csr, filename, filename_y, filename_test)
+    write_csr_to_disk(neg_training_csr, filename_neg)
+    write_csr_to_disk(pos_training_csr, filename_pos)
+    write_csr_to_disk(test_csr, filename_test)
+    write_list_to_disk(neg_binding_vector, filename_y_neg )
+    write_list_to_disk(pos_binding_vector, filename_y_pos )
 
 if __name__ == '__main__':
     main()
